85652b2de744692e6e304062841cc62ba5aa2d72
Nathan Wong <nathan.wong2002@gmail.com>
2024-02-08 15:21:06 -0700

    main CSS structure done

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('test');
+const yes_btn = document.querySelector("#yes-btn");
+const no_btn = document.querySelector("#no-btn");
+
+no_btn.addEventListener("click", () =>{ // On click => Show the dramatic cat meme.
+    console.log('noo :(');
+})
+
+yes_btn.addEventListener("click", () => { // On click => Show the happy happy cat meme.
+    console.log('YAHOO');
+
+})
