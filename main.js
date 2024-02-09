149556687991ed98af4c11da32c0184bf2ea0b27
Nathan Wong <nathan.wong2002@gmail.com>
2024-02-08 18:28:08 -0700

    adjusted button behaviours

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ var happy_song = document.querySelector("#happy-song");
 
 no_btn.addEventListener("click", () =>{ // On click => Show the dramatic cat meme.
     no_dialog.style.display = 'flex';
+    yes_btn.classList.add("btn-disabled"); // User can't click "yes" btn after opening a "no-dialog".
 })
 
 yes_btn.addEventListener("click", () => { // On click => Show the happy happy cat meme.
+
     yes_dialog.style.display = 'flex';
     happy_song.play();
+    no_btn.style.display = 'none'; // Removes the "no-btn" lmao.
 })
 
 close_btn_no.addEventListener("click", () => {
     no_dialog.style.display = 'none';
+    yes_btn.classList.remove("btn-disabled"); // Re-enables the "yes" button.
 })
 
 close_btn_yes.addEventListener("click", () => {
