22c60f648b1cc65206535c4869aa216f0bb8f8e7
Nathan Wong <nathan.wong2002@gmail.com>
2024-02-08 18:05:28 -0700

    complete LOL

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const yes_btn = document.querySelector("#yes-btn");
 const no_btn = document.querySelector("#no-btn");
+const yes_dialog = document.querySelector(".yes-dialog");
+const no_dialog = document.querySelector(".no-dialog");
+const close_btn_no = document.querySelector(".close-btn-no");
+const close_btn_yes = document.querySelector(".close-btn-yes");
+var happy_song = document.querySelector("#happy-song");
 
 no_btn.addEventListener("click", () =>{ // On click => Show the dramatic cat meme.
-    console.log('noo :(');
+    no_dialog.style.display = 'flex';
 })
 
 yes_btn.addEventListener("click", () => { // On click => Show the happy happy cat meme.
-    console.log('YAHOO');
+    yes_dialog.style.display = 'flex';
+    happy_song.play();
+})
 
+close_btn_no.addEventListener("click", () => {
+    no_dialog.style.display = 'none';
+})
+
+close_btn_yes.addEventListener("click", () => {
+    yes_dialog.style.display = 'none';
+    happy_song.pause();
 })
